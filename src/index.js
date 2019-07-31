2484636e5d2814144fdafde4667056c21bb7625a
Jaid <jaid.jsx@gmail.com>
2019-07-31 04:33:43 +0200

    Finished constructor
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@
 import jaidLogger from "jaid-logger"
 import camelCase from "camelcase"
 import essentialConfig from "essential-config"
-
+import hasContent from "has-content"
+import Sequelize from "sequelize"
+import {isString} from "lodash"
 /**
  * @typedef {Object} Options
  * @prop {string} name
  * @prop {string|string[]} [folder]
  * @prop {string} version
- * @prop {Object} configSetup
+ * @prop {import("essential-config").Options} configSetup
  * @prop {boolean|string} database
+ * @prop {Object<string, *>} sequelizeOptions
  */
 
 /**
@@ -31,6 +34,7 @@ export default class {
       configSetup: {},
       ...options,
     }
+    const hasDatabase = Boolean(options.database)
     /**
      * @type {string}
      */
@@ -57,16 +61,52 @@ export default class {
     if (options.configSetup.sensitiveKeys === undefined) {
       options.configSetup.sensitiveKeys = []
     }
-    if (options.database) {
+    if (hasDatabase) {
       Object.assign(options.configSetup.defaults, {
-        databaseName: this.camelName,
+        databaseName: isString(options.database) ? options.database : this.camelName,
         databaseUser: "postgres",
         databaseHost: "localhost",
         databasePort: 5432,
+        timezone: "Europe/Berlin",
       })
       options.configSetup.sensitiveKeys.push("databasePassword")
     }
     const configResult = essentialConfig(options.configSetup)
+    if (!configResult.config) {
+      this.logger.warn("Set up default config at %s, please edit and restart!", configResult.configFile)
+      process.exit(2)
+    }
+    if (configResult.newKeys |> hasContent) {
+      this.logger.info("Added new keys to config file %s: %s", configResult.config, configResult.newKeys.join(", "))
+    }
+    /**
+     * @type {string}
+     */
+    this.configFile = configResult.configFile
+    /**
+     * @type {Object<string, *>}
+     */
+    this.config = configResult.config
+    if (hasDatabase) {
+      this.database = new Sequelize({
+        dialect: "postgres",
+        host: this.config.databaseHost,
+        port: this.config.databasePort,
+        database: this.config.databaseName,
+        username: this.config.databaseUser,
+        password: this.config.databasePassword,
+        timezone: this.config.timezone,
+        benchmark: true,
+        logging: line => {
+          this.logger.debug(line)
+        },
+        ...options.sequelizeOptions,
+      })
+    }
+  }
+
+  async init() {
+
   }
 
 }
