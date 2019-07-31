8a17a56b458099dfe13a65367b70ef1cb8d8492d
Jaid <jaid.jsx@gmail.com>
2019-07-31 03:35:24 +0200

    Added some stubs
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,72 @@
 /** @module jaid-core */
 
+import jaidLogger from "jaid-logger"
+import camelCase from "camelcase"
+import essentialConfig from "essential-config"
+
 /**
- * @function
- * @param {number} [compareValue]
- * @returns {number} Seconds passed since Unix epoch (01 January 1970)
- * @example
- * import jaidCore from "jaid-core"
- * const result = jaidCore()
- * result === 1549410770
- * setTimeout(() => {
- *   const result2 = jaidCore(result)
- *   result2 === 3
- * }, 3000)
+ * @typedef {Object} Options
+ * @prop {string} name
+ * @prop {string|string[]} [folder]
+ * @prop {string} version
+ * @prop {Object} configSetup
+ * @prop {boolean|string} database
  */
-export default compareValue => {
-  const seconds = Math.floor(Date.now() / 1000)
-  if (compareValue === undefined) {
-    return seconds
-  } else {
-    return seconds - compareValue
+
+/**
+ * @class
+ */
+export default class {
+
+  /**
+   * @constructor
+   * @param {Options} options
+   */
+  constructor(options) {
+    /**
+     * @type {Date}
+     */
+    this.startTime = new Date()
+    options = {
+      configSetup: {},
+      ...options,
+    }
+    /**
+     * @type {string}
+     */
+    this.camelName = options.name |> camelCase
+    /**
+     * @type {string[]}
+     */
+    const appPath = [...options.folder || [], options.name]
+    /**
+     * @type {import("jaid-logger").JaidLogger}
+     */
+    this.logger = jaidLogger(appPath)
+    /**
+     * @type {string}
+     */
+    this.appFolder = this.logger.appFolder
+    /**
+     * @type {string}
+     */
+    this.logFolder = this.logger.logFolder
+    if (options.configSetup.defaults === undefined) {
+      options.configSetup.defaults = {}
+    }
+    if (options.configSetup.sensitiveKeys === undefined) {
+      options.configSetup.sensitiveKeys = []
+    }
+    if (options.database) {
+      Object.assign(options.configSetup.defaults, {
+        databaseName: this.camelName,
+        databaseUser: "postgres",
+        databaseHost: "localhost",
+        databasePort: 5432,
+      })
+      options.configSetup.sensitiveKeys.push("databasePassword")
+    }
+    const configResult = essentialConfig(options.configSetup)
   }
+
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import path from "path"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
@@ -5,9 +7,11 @@ const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.jo
 /**
  * @type { import("../src") }
  */
-const {default: jaidCore} = indexModule
+const {default: JaidCore} = indexModule
 
 it("should run", () => {
-  const result = jaidCore()
+  const result = new JaidCore({
+    name: 2,
+  })
   expect(result).toBeGreaterThan(1549410770)
 })
