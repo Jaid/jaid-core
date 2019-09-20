4322998b4de7f9f7482f293e2147a2b887d43ee8
Jaid <jaid.jsx@gmail.com>
2019-09-20 02:33:28 +0200

    feat: Added callable `handleConfig` for plugins
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -342,6 +342,7 @@ export default class {
         this.logger.warn("Config contains %s: %s", zahl(configResult.deprecatedKeys, "no longer needed entry"), configResult.deprecatedKeys.join(", "))
       }
       Object.assign(this.config, configResult.config)
+      await this.callAndRemovePlugins("handleConfig")
       if (this.hasDatabase) {
         const Sequelize = __non_webpack_require__("sequelize")
         const sequelizeOptions = {}
@@ -486,7 +487,7 @@ export default class {
           }
         }
       }
-      await this.callPlugins("init")
+      await this.call("init")
       if (this.hasInsecureServer) {
         this.insecureServer.listen(this.config.insecurePort)
         this.logger.info("Started insecure server on port %s", this.config.insecurePort)
